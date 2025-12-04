3c6aeec0d2a82caead284d3c024203d0e128e386
a-muratov <a-muratov@users.noreply.github.com>
2025-12-04 19:34:11 +0100

    Solve day 4, part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,7 +1,7 @@
 data class RollGrid(val input: List<String>) {
     val width = input[0].length
     val height = input.size
-    val grid = input.map { it.toList() }
+    val grid = input.map { it.toMutableList() }
     fun isRoll(i: Int, j: Int) = (grid[i][j] == '@')
     fun neighbors(i: Int, j: Int) =
         listOf(
@@ -21,21 +21,33 @@ data class RollGrid(val input: List<String>) {
 
     fun nNeighborRolls(i: Int, j: Int) = neighbors(i, j).count { isRoll(it.first, it.second) }
 
-    fun nAccessibleRolls() = (0..<height).sumOf { i ->
-        (0..<width).count { j ->
-            isRoll(i, j) && nNeighborRolls(i, j) < 4
+    fun accessibleRolls() = (0..<height).flatMap { i ->
+        (0..<width).map { j ->
+            i to j
         }
+    }.filter{(i, j) -> isRoll(i, j) && nNeighborRolls(i, j) < 4}
+
+    fun removeAccessibleRolls(): Int {
+        val accessibleRolls = accessibleRolls()
+        accessibleRolls.forEach { (i, j) -> grid[i][j] = '.' }
+        return accessibleRolls.size
     }
 }
 
 fun main() {
     fun part1(input: List<String>): Int {
         val rollGrid = RollGrid(input)
-        return rollGrid.nAccessibleRolls()
+        return rollGrid.accessibleRolls().size
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val rollGrid = RollGrid(input)
+        var nRollsRemoved = 0
+        do {
+            val moreRollsRemoved = rollGrid.removeAccessibleRolls()
+            nRollsRemoved += moreRollsRemoved
+        } while (moreRollsRemoved > 0)
+        return nRollsRemoved
     }
 
     val input = readInput("day04_input.txt")
@@ -58,6 +70,38 @@ fun main() {
         ) == 13
     ) {
         part1(
+            listOf(
+                "..@@.@@@@.",
+                "@@@.@.@.@@",
+                "@@@@@.@.@@",
+                "@.@@@@..@.",
+                "@@.@@@@.@@",
+                ".@@@@@@@.@",
+                ".@.@.@.@@@",
+                "@.@@@.@@@@",
+                ".@@@@@@@@.",
+                "@.@.@@@.@."
+            )
+        ).toString()
+    }
+
+    check(
+        part2(
+            listOf(
+                "..@@.@@@@.",
+                "@@@.@.@.@@",
+                "@@@@@.@.@@",
+                "@.@@@@..@.",
+                "@@.@@@@.@@",
+                ".@@@@@@@.@",
+                ".@.@.@.@@@",
+                "@.@@@.@@@@",
+                ".@@@@@@@@.",
+                "@.@.@@@.@."
+            )
+        ) == 43
+    ) {
+        part2(
             listOf(
                 "..@@.@@@@.",
                 "@@@.@.@.@@",
